d71fe3933555c30f9111fb50de79e31b7d962857
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-02-26 21:49:32 -0600

    User can tap a cell to see more details about a particular movie.

--- Flix_Clone/ViewController.swift
+++ Flix_Clone/ViewController.swift
@@ -23,14 +23,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         tableView.delegate = self
          
-        print("Hello World")
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
              // This will run when the network request returns
              if let error = error {
-                    print(error.localizedDescription)
+                     //print(error.localizedDescription)
              } else if let data = data {
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
 
@@ -70,6 +70,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell;
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("loading up the screen detail")
+        
+        // find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        // Pass the selected movie into the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 
 
 }
